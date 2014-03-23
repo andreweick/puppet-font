545612610351bd124ad90ae14698476b2b9fb88c
Andrew Eick <maeick@missionfocus.com>
2014-03-23 15:08:28 -0400

    changed private fonts into directory

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,105 +44,108 @@ class font::source-code-pro {
   }
 }
 
+
+#Private Fonts
+
 class font::comicbookfonts {
   file { 'CCBryanTalbotBoldItalic.otf':
     path   => '/Users/maeick/Library/Fonts/CCBryanTalbotBoldItalic.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/BryanTalbot-Intl/CCBryanTalbotBoldItalic.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/BryanTalbot-Intl/CCBryanTalbotBoldItalic.otf'
   }
   file { 'CCBryanTalbotItalic.otf':
     path   => '/Users/maeick/Library/Fonts/CCBryanTalbotItalic.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/BryanTalbot-Intl/CCBryanTalbotItalic.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/BryanTalbot-Intl/CCBryanTalbotItalic.otf'
   }
   file { 'CCBryanTalbotLowerBolIta.otf':
     path   => '/Users/maeick/Library/Fonts/CCBryanTalbotLowerBolIta.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/BryanTalbot-Intl/CCBryanTalbotLowerBolIta.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/BryanTalbot-Intl/CCBryanTalbotLowerBolIta.otf'
   }
   file { 'CCBryanTalbotLowerItalic.otf':
     path   => '/Users/maeick/Library/Fonts/CCBryanTalbotLowerItalic.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/BryanTalbot-Intl/CCBryanTalbotLowerItalic.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/BryanTalbot-Intl/CCBryanTalbotLowerItalic.otf'
   }
   file { 'CCBryanTalbotLowerRegular.otf':
     path   => '/Users/maeick/Library/Fonts/CCBryanTalbotLowerRegular.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/BryanTalbot-Intl/CCBryanTalbotLowerRegular.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/BryanTalbot-Intl/CCBryanTalbotLowerRegular.otf'
   }
   file { 'CCBryanTalbotRegular.otf':
     path   => '/Users/maeick/Library/Fonts/CCBryanTalbotRegular.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/BryanTalbot-Intl/CCBryanTalbotRegular.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/BryanTalbot-Intl/CCBryanTalbotRegular.otf'
   }
 
   file { 'CCComicrazy-Bold.otf':
     path   => '/Users/maeick/Library/Fonts/CCComicrazy-Bold.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/Comicrazy/CCComicrazy-Bold.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/Comicrazy/CCComicrazy-Bold.otf'
   }
   file { 'CCComicrazy-BoldItalic.otf':
     path   => '/Users/maeick/Library/Fonts/CCComicrazy-BoldItalic.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/Comicrazy/CCComicrazy-BoldItalic.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/Comicrazy/CCComicrazy-BoldItalic.otf'
   }
   file { 'CCComicrazy-Italic.otf':
     path   => '/Users/maeick/Library/Fonts/CCComicrazy-Italic.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/Comicrazy/CCComicrazy-Italic.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/Comicrazy/CCComicrazy-Italic.otf'
   }
   file { 'CCComicrazy-Regular.otf':
     path   => '/Users/maeick/Library/Fonts/CCComicrazy-Regular.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/Comicrazy/CCComicrazy-Regular.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/Comicrazy/CCComicrazy-Regular.otf'
   }
 
   file { 'CCDashDecent-Bold.otf':
     path   => '/Users/maeick/Library/Fonts/CCDashDecent-Bold.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/DashDecent-Intl/CCDashDecent-Bold.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/DashDecent-Intl/CCDashDecent-Bold.otf'
   }
   file { 'CCDashDecent-BoldItalic.otf':
     path   => '/Users/maeick/Library/Fonts/CCDashDecent-BoldItalic.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/DashDecent-Intl/CCDashDecent-BoldItalic.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/DashDecent-Intl/CCDashDecent-BoldItalic.otf'
   }
   file { 'CCDashDecent-Heavy.otf':
     path   => '/Users/maeick/Library/Fonts/CCDashDecent-Heavy.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/DashDecent-Intl/CCDashDecent-Heavy.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/DashDecent-Intl/CCDashDecent-Heavy.otf'
   }
   file { 'CCDashDecent-HeavyItalic.otf':
     path   => '/Users/maeick/Library/Fonts/CCDashDecent-HeavyItalic.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/DashDecent-Intl/CCDashDecent-HeavyItalic.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/DashDecent-Intl/CCDashDecent-HeavyItalic.otf'
   }
   file { 'CCDashDecent-Italic.otf':
     path   => '/Users/maeick/Library/Fonts/CCDashDecent-Italic.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/DashDecent-Intl/CCDashDecent-Italic.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/DashDecent-Intl/CCDashDecent-Italic.otf'
   }
   file { 'CCDashDecent-Regular.otf':
     path   => '/Users/maeick/Library/Fonts/CCDashDecent-Regular.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/DashDecent-Intl/CCDashDecent-Regular.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/DashDecent-Intl/CCDashDecent-Regular.otf'
   }
 
   file { 'CCSpellcasterItalic.otf':
     path   => '/Users/maeick/Library/Fonts/CCSpellcasterItalic.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/Spellcaster/CCSpellcasterItalic.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/Spellcaster/CCSpellcasterItalic.otf'
   }
   file { 'CCSpellcasterRegular.otf':
     path   => '/Users/maeick/Library/Fonts/CCSpellcasterRegular.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/Spellcaster/CCSpellcasterRegular.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/Spellcaster/CCSpellcasterRegular.otf'
   }
 
   file { 'CCSpills.otf':
     path   => '/Users/maeick/Library/Fonts/CCSpills.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/comicbookfonts/Spills/CCSpills.otf'
+    source => 'puppet:///modules/font/private-fonts/comicbookfonts/Spills/CCSpills.otf'
   }
 }
 
@@ -150,97 +153,97 @@ class font::hoefler {
   file { 'Landmark-Shadow.otf':
     path   => '/Users/maeick/Library/Fonts/Landmark-Shadow.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/landmark/Landmark-Shadow.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/landmark/Landmark-Shadow.otf'
   }
   file { 'Landmark-Regular.otf':
     path   => '/Users/maeick/Library/Fonts/Landmark-Regular.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/landmark/Landmark-Regular.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/landmark/Landmark-Regular.otf'
   }
   file { 'Landmark-Inline.otf':
     path   => '/Users/maeick/Library/Fonts/Landmark-Inline.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/landmark/Landmark-Inline.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/landmark/Landmark-Inline.otf'
   }
   file { 'Landmark-Dimensional.otf':
     path   => '/Users/maeick/Library/Fonts/Landmark-Dimensional.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/landmark/Landmark-Dimensional.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/landmark/Landmark-Dimensional.otf'
   }
   file { 'Idlewild-Thin.otf':
     path   => '/Users/maeick/Library/Fonts/Idlewild-Thin.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/idlewild/Idlewild-Thin.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/idlewild/Idlewild-Thin.otf'
   }
   file { 'Idlewild-Medium.otf':
     path   => '/Users/maeick/Library/Fonts/Idlewild-Medium.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/idlewild/Idlewild-Medium.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/idlewild/Idlewild-Medium.otf'
   }
   file { 'Idlewild-Light.otf':
     path   => '/Users/maeick/Library/Fonts/Idlewild-Light.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/idlewild/Idlewild-Light.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/idlewild/Idlewild-Light.otf'
   }
   file { 'Idlewild-Book.otf':
     path   => '/Users/maeick/Library/Fonts/Idlewild-Book.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/idlewild/Idlewild-Book.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/idlewild/Idlewild-Book.otf'
   }
   file { 'Idlewild-Bold.otf':
     path   => '/Users/maeick/Library/Fonts/Idlewild-Bold.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/idlewild/Idlewild-Bold.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/idlewild/Idlewild-Bold.otf'
   }
   file { 'IdealSans-SemiboldItalic-Pro.otf':
     path   => '/Users/maeick/Library/Fonts/IdealSans-SemiboldItalic-Pro.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/IdealSans/IdealSans-SemiboldItalic-Pro.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/IdealSans/IdealSans-SemiboldItalic-Pro.otf'
   }
   file { 'IdealSans-Semibold-Pro.otf':
     path   => '/Users/maeick/Library/Fonts/IdealSans-Semibold-Pro.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/IdealSans/IdealSans-Semibold-Pro.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/IdealSans/IdealSans-Semibold-Pro.otf'
   }
   file { 'IdealSans-MediumItalic-Pro.otf':
     path   => '/Users/maeick/Library/Fonts/IdealSans-MediumItalic-Pro.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/IdealSans/IdealSans-MediumItalic-Pro.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/IdealSans/IdealSans-MediumItalic-Pro.otf'
   }
   file { 'IdealSans-Medium-Pro.otf':
     path   => '/Users/maeick/Library/Fonts/IdealSans-Medium-Pro.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/IdealSans/IdealSans-Medium-Pro.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/IdealSans/IdealSans-Medium-Pro.otf'
   }
   file { 'IdealSans-LightItalic-Pro.otf':
     path   => '/Users/maeick/Library/Fonts/IdealSans-LightItalic-Pro.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/IdealSans/IdealSans-LightItalic-Pro.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/IdealSans/IdealSans-LightItalic-Pro.otf'
   }
   file { 'IdealSans-Light-Pro.otf':
     path   => '/Users/maeick/Library/Fonts/IdealSans-Light-Pro.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/IdealSans/IdealSans-Light-Pro.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/IdealSans/IdealSans-Light-Pro.otf'
   }
   file { 'IdealSans-BookItalic-Pro.otf':
     path   => '/Users/maeick/Library/Fonts/IdealSans-BookItalic-Pro.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/IdealSans/IdealSans-BookItalic-Pro.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/IdealSans/IdealSans-BookItalic-Pro.otf'
   }
   file { 'IdealSans-Book-Pro.otf':
     path   => '/Users/maeick/Library/Fonts/IdealSans-Book-Pro.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/IdealSans/IdealSans-Book-Pro.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/IdealSans/IdealSans-Book-Pro.otf'
   }
   file { 'IdealSans-BoldItalic-Pro.otf':
     path   => '/Users/maeick/Library/Fonts/IdealSans-BoldItalic-Pro.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/IdealSans/IdealSans-BoldItalic-Pro.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/IdealSans/IdealSans-BoldItalic-Pro.otf'
   }
   file { 'IdealSans-Bold-Pro.otf':
     path   => '/Users/maeick/Library/Fonts/IdealSans-Bold-Pro.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/hoefler/IdealSans/IdealSans-Bold-Pro.otf'
+    source => 'puppet:///modules/font/private-fonts/hoefler/IdealSans/IdealSans-Bold-Pro.otf'
   }
 }
 
@@ -248,7 +251,7 @@ class font::myfonts {
   file { 'bemis.otf':
     path   => '/Users/maeick/Library/Fonts/bemis.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/myfonts/bemis/bemis.otf'
+    source => 'puppet:///modules/font/private-fonts/myfonts/bemis/bemis.otf'
   }
 }
 
@@ -256,11 +259,11 @@ class font::sketchnote {
   file { 'Sketchnote-Dingbats.otf':
     path   => '/Users/maeick/Library/Fonts/Sketchnote-Dingbats.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/sketchnote/Sketchnote-Dingbats.otf'
+    source => 'puppet:///modules/font/private-fonts/sketchnote/Sketchnote-Dingbats.otf'
   }
   file { 'Sketchnote-Square.otf':
     path   => '/Users/maeick/Library/Fonts/Sketchnote-Square.otf',
     ensure => 'present',
-    source => 'puppet:///modules/font/sketchnote/Sketchnote-Square.otf'
+    source => 'puppet:///modules/font/private-fonts/sketchnote/Sketchnote-Square.otf'
   }
 }
